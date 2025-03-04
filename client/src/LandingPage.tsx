f3eb645349b3dd99c5df7fd2241d782a4ff80278
Krystal Jane Siaotong <krystaljane.siaotong-23@cpu.edu.ph>
2025-03-04 20:55:06 +0800

    undo landing

--- client/src/LandingPage.tsx
+++ client/src/LandingPage.tsx
@@ -1,3 +1,15 @@
+// import Magic8Ball from "./Magic8Ball";
+
+// const LandingPage = () => {
+//   return (
+//     <div className="flex flex-col items-center min-h-screen w-screen h-screen justify-center">
+//       <Magic8Ball />
+//     </div>
+//   );
+// };
+
+// export default LandingPage;
+
 import { useEffect, useState } from 'react';
 import DisplayComp from './Components/Input';
 import FilterComp from './Components/Button';
